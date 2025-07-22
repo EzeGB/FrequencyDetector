3a2e860dabf38f35e00783c62762b61713d68514
Zeke <ezequielgerstel777@gmail.com>
2025-07-22 14:21:35 -0400

    Code clean up III all logic cleaned

--- app/src/main/java/com/example/frequencydetector/MainActivity.kt
+++ app/src/main/java/com/example/frequencydetector/MainActivity.kt
@@ -2,25 +2,16 @@ package com.example.frequencydetector
 
 import android.Manifest
 import android.content.pm.PackageManager
-import android.media.AudioFormat
-import android.media.AudioRecord
-import android.media.MediaRecorder
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.os.HandlerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import be.hogent.tarsos.dsp.MicrophoneAudioDispatcher
 import be.hogent.tarsos.dsp.pitch.PitchDetectionHandler
 import be.hogent.tarsos.dsp.pitch.PitchProcessor
 import com.example.frequencydetector.databinding.ActivityMainBinding
-import java.io.IOException
-import java.lang.Exception
 
 const val REQUEST_CODE = 200
 class MainActivity : AppCompatActivity() {
@@ -29,15 +20,7 @@ class MainActivity : AppCompatActivity() {
     private var recordAudioPermissions = arrayOf(Manifest.permission.RECORD_AUDIO)
     private var recordAudioPermissionsGranted = false
 
-    private lateinit var handler: Handler
-    private lateinit var runnable: Runnable
-
-    private lateinit var mediaRecorder: MediaRecorder
     private val audioThread = setUpPitchDetectionAudioThread()
-
-    private var recording = false
-    private lateinit var dirPath : String
-    private var filename = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,100 +39,29 @@ class MainActivity : AppCompatActivity() {
         recordAudioPermissionsGranted = ActivityCompat.checkSelfPermission(this,recordAudioPermissions[0])==PackageManager.PERMISSION_GRANTED
         checkForPermissions()
 
-        handler = HandlerCompat.createAsync(mainLooper)
-        runnable = Runnable { runCoreLoop() }
-        dirPath = "${externalCacheDir?.absolutePath}/"
-
-        binding.initialController.setOnClickListener(View.OnClickListener {
-            if (checkForPermissions()){
-                if (recording){
-                    stopRecording()
-                } else {
-                    startRecording()
-                }
-            }
-        })
-    }
-
-    private fun processPitch(pitchInHz: Float){
-        binding.textFrequency.text = pitchInHz.toString()
-    }
-
-    private fun startRecording(){
-        binding.initialController.setBackgroundResource(R.drawable.icon_controller_on)
-        binding.textFrequency.text = filename
-        handler.postDelayed(runnable,0L)
         audioThread.start()
-    }
-
-    private fun stopRecording(){
-        binding.initialController.setBackgroundResource(R.drawable.icon_controller_off)
-        handler.removeCallbacks(runnable)
-        tryToStopMediaRecorder()
-        recording = false
-        //binding.textFrequency.text = "Stopped"
-        audioThread.interrupt()
-    }
-
-    private fun runCoreLoop (){
-        if (recording){
-            tryToStopMediaRecorder()
-            recording = false
-        }
-        setUpMediaRecorder()
-        mediaRecorder.apply {
-            setOutputFile("$dirPath${renameFilename()}.mp3")
-            try {
-                prepare()
-            }catch (e:IOException){}
-            start()
-            recording = true
-        }
-        //binding.textFrequency.text = "Recording: ".plus(filename)
-        handler.postDelayed(runnable,1000L)
-    }
-
-    private fun setUpMediaRecorder(){
-        mediaRecorder = MediaRecorder()
-        mediaRecorder.apply {
-            setAudioSource(MediaRecorder.AudioSource.MIC)
-            setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
-            setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
-            setAudioSamplingRate(44100)
-            setAudioEncodingBitRate(320000)
-        }
     }
 
     private fun setUpPitchDetectionAudioThread () : Thread {
         val dispatcher = MicrophoneAudioDispatcher(22050, 1024, 0)
 
-        val pdh = PitchDetectionHandler { pitchDetectionResult, audioEvent ->
+        val pdHandler = PitchDetectionHandler { pitchDetectionResult, audioEvent ->
             val pitchInHz = pitchDetectionResult.pitch
             runOnUiThread{
-                processPitch(pitchInHz)
+                updatePitchText(pitchInHz)
             }
         }
-        val pitchProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050F,1024,pdh)
+
+        val pitchProcessor = PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN,
+            22050F,1024,pdHandler)
+
         dispatcher.addAudioProcessor(pitchProcessor)
+
         return Thread(dispatcher,"Audio Thread")
     }
 
-    private fun renameFilename() : String{
-        if (filename==""||filename=="TempXXXX"){
-            filename = "Temp"
-        }else{
-            filename = filename.plus("X")
-        }
-        return filename
-    }
-
-    private fun tryToStopMediaRecorder(){
-        try {
-            mediaRecorder.stop()
-            mediaRecorder.release()
-        }catch (e:Exception){
-            Log.d("Debug4","already stopped")
-        }
+    private fun updatePitchText(pitchInHz: Float){
+        binding.textFrequency.text = pitchInHz.toString()
     }
 
     override fun onRequestPermissionsResult(
